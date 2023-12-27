24aef56a402c1e6a36c0cd140e2830e36c085c9e
ishaanps <ishaanps92@gmail.com>
2023-12-27 21:58:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 document.getElementById('startButton').addEventListener('click', function() {
     let count = 0;
     let reps = 0;
+    let timerDisplay = document.getElementById('timer');
     let interval = setInterval(() => {
-        count++;
-        if (count % 6 === 0) {
+        let timeInRep = count % 6;
+        timerDisplay.textContent = timeInRep === 0 ? 5 : timeInRep - 1; // Update the timer display
+
+        if (timeInRep === 0) {
             beep();
             reps++;
             document.getElementById('reps').textContent = reps;
-        } else if (count % 6 === 5) {
+        } else if (timeInRep === 5) {
             beep();
         }
+
+        count++;
     }, 1000);
 });
 
 function beep() {
-    var snd = new Audio('beep.wav');
+    var snd = new Audio('beep.wav'); // Make sure the path to your audio file is correct
     snd.play();
 }
